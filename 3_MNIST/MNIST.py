c084b7058a8bd3519d99ed8840e92a3b09ad52f0
Gauraviitkgp <gauravs123456789@gmail.com>
2019-06-14 14:21:32 -0400

    New tests Ran

--- 3_MNIST/MNIST.py
+++ 3_MNIST/MNIST.py
@@ -6,11 +6,12 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D, Input
 from uniform_noise import UniformNoise
 from keras import backend as K
-
+from math import pi,e,sqrt
 batch_size = 128
 num_classes = 10
 epochs = 12
-
+b=0.05
+a=-0.05
 # input image dimensions
 img_rows, img_cols = 28, 28
 
@@ -20,13 +21,13 @@ img_rows, img_cols = 28, 28
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
     x_test  = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    # input_shape = (1, img_rows, img_cols)
-    input_shape = Input((1, img_rows, img_cols))
+    input_shape = (1, img_rows, img_cols)
+    # input_shape = Input((1, img_rows, img_cols))
 else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
     x_test  = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    # input_shape = (img_rows, img_cols, 1)
-    input_shape = Input((img_rows, img_cols, 1))
+    input_shape = (img_rows, img_cols, 1)
+    # input_shape = Input((img_rows, img_cols, 1))
 
 x_train = x_train.astype('float32') 
 x_test = x_test.astype('float32')
@@ -41,29 +42,30 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-# model.add(Conv2D(32, kernel_size=(3, 3),
-#                  activation='relu',
-#                  input_shape=input_shape)) #Convultional Layer
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2))) # Reduce dimensionality of the image
-# # model.add(Dropout(0.25)) #Dropout Probablity
-# model.add(keras.layers.GaussianNoise(0.05/3))
-# model.add(Flatten())
-# model.add(Dense(128, activation='relu'))
-# # model.add(Dropout(0.5))
-# model.add(keras.layers.GaussianNoise(0.05/3))
-# model.add(Dense(num_classes, activation='softmax'))
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape)) #Convultional Layer
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2))) # Reduce dimensionality of the image
+# model.add(Dropout(0.25)) #Dropout Probablity
+# common_variance=((b-a)**2)/12 #Common Variance
+common_entropy=(b-a)/sqrt(2*pi*e)
+model.add(keras.layers.GaussianNoise(common_entropy))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+model.add(keras.layers.GaussianNoise(common_entropy))
+model.add(Dense(num_classes, activation='softmax'))
 
-conv1   = Conv2D(32, kernel_size=(3, 3), activation='relu')(input_shape)
-conv2   = Conv2D(64, (3, 3), activation='relu')(conv1)
-maxpool = MaxPooling2D(pool_size=(2, 2))(conv2)
-conv2n  = UniformNoise(minval=-0.05, maxval=0.05)(maxpool)
-fconv2n = Flatten()(conv2n)
-layer3  = Dense(128, activation='relu')(fconv2n)
-layer3n = UniformNoise(minval=-0.05, maxval=0.05)(layer3)
-out     = Dense(num_classes, activation='softmax')(layer3n)
-
-model = Model(inputs=input_shape, outputs=out)
+# conv1   = Conv2D(32, kernel_size=(3, 3), activation='relu')(input_shape)
+# conv2   = Conv2D(64, (3, 3), activation='relu')(conv1)
+# maxpool = MaxPooling2D(pool_size=(2, 2))(conv2)
+# conv2n  = UniformNoise(minval=-0.05, maxval=0.05)(maxpool)
+# fconv2n = Flatten()(conv2n)
+# layer3  = Dense(128, activation='relu')(fconv2n)
+# layer3n = UniformNoise(minval=-0.05, maxval=0.05)(layer3)
+# out     = Dense(num_classes, activation='softmax')(layer3n)
+# model = Model(inputs=input_shape, outputs=out)
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
